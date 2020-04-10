6b317a63caeae8d7981ef05b42fb11b0a37a9161
JS41706744 <896190606@qq.com>
2020-04-10 12:05:00 +0800

    Add fragment.

--- app/src/main/java/com/coolweather/android/db/City.java
+++ app/src/main/java/com/coolweather/android/db/City.java
@@ -10,7 +10,15 @@ public class City extends DataSupport {
 
     private int cityCode;
 
-    private int provinceCode;
+    private int provinceId;
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
 
     public int getId() {
         return id;
@@ -36,11 +44,5 @@ public class City extends DataSupport {
         this.cityCode = cityCode;
     }
 
-    public int getProvinceCode() {
-        return provinceCode;
-    }
 
-    public void setProvinceCode(int provinceCode) {
-        this.provinceCode = provinceCode;
-    }
 }
