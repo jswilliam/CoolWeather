f352217674eedaf4d1f269c75b732837eb950c0c
jswilliam <896190606@qq.com>
2020-04-27 08:43:30 +0800

    weather23

---
+++ app/src/main/java/com/coolweather/android/WeatherActivity.java
@@ -0,0 +1,16 @@
+package com.coolweather.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.coolweather.R;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+    }
+}
